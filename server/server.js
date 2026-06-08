54e8ed053df633ac16e27b5e81888bb76707dd0b
Sargam-Maurya <mauryasargam39@gmail.com>
2026-06-08 17:58:22 +0530

    backend ready for deployment

--- server/server.js
+++ server/server.js
@@ -14,12 +14,22 @@ const errorMiddleware = require("./middlewares/error-middleware");
 
 
 // handling cors /
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
+//   credentials: true,
+// }//cr-2
+// app.use(cors(corsOptions));//cr-3
+
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://mern-course-website.vercel.app'
+  ],
   methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
   credentials: true,
-}//cr-2
-app.use(cors(corsOptions));//cr-3
+};
+app.use(cors(corsOptions))
 
 app.use(express.json()) //middleware hai ye 
 
@@ -29,7 +39,8 @@ app.use("/api/data", serviceRoute); // Service -4
 
 app.use(errorMiddleware) // hamesha end main add karna hai db connection ke pehele errorMiddleware use karna hai toh
 
-const PORT = 5000
+// const PORT = 5000; //port number set kiya hai ya toh env file se lega ya 5000 lega
+const PORT = process.env.PORT || 5000;
 connectDb().then(()=>{
     app.listen(PORT, () => {
     console.log(`Server is running on port number ${PORT}`)
